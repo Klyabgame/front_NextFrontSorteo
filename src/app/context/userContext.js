8804e512870bd749875077ed36aef9f29d9fc58a
franz <gavino_10@hotmail.es>
2024-03-23 14:28:55 -0500

    fix:editando usercontext

--- src/app/context/userContext.js
+++ src/app/context/userContext.js
@@ -4,14 +4,17 @@ import { createContext, useEffect, useState } from "react";
 export const UserContext = createContext({});
 
 export function UserContextProvider({ children }) {
+
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    if (!user) {
+    if (user===null) {
       fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/usuario/validate-token`, {
         method: "GET",
         headers: {
           "Content-type": "application/json",
+          "Authorization": `Bearer ${document.cookie.replace(/(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/, "$1")}`
+        
         },
         credentials: "include",
       })
@@ -19,7 +22,7 @@ export function UserContextProvider({ children }) {
         .then((data) => setUser(data))
         .catch((error) => console.error("Error fetching user:", error));
     }
-  }, [user]);
+  }, []);
 
   return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
 }
